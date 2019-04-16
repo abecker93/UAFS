c4ccf0db74cfa29bc42dbdc257879980812724b0
Andrew Becker <ajbecker@uvm.edu>
2019-04-16 10:53:05 -0400

    Updated uafs_pick to correctly transform factors into 0-1 dummy variables

--- UAFS_pack/R/uafs_pick.R
+++ UAFS_pack/R/uafs_pick.R
@@ -11,8 +11,34 @@ uafs.pick <- function(data, output, alpha=0.025){
   n <- ncol(data)
   classes <- sapply(data, class)
   yclass <- class(output)
+  
+  for(i in 1:n){ #this loop converts all factors into 0-1 dummy variables if they in 'data'
+    if(classes[i]=='factor'){
+      levels <- length(levels(data[,i]))
+      data_check <- as.numeric(data[,i])
+      data_new <- numeric(0)
+      for(j in 1:(levels-1)){
+        data_temp <- rep(0, nrow(data))
+        data_temp[data_check==j] <- 1
+        data_new <- cbind(data_new, data_temp)
+        colnames(data_new)[j] <- paste('V',i,'-',j, sep='')
+      }
+      data <- cbind(data, data_new)
+    }
+  }
+  
+  data <- data[,-which(classes=='factor')] #remove remaining factors
+  
+  if(yclass=='factor' & length(levels(output))==2){ #converts binary factor outcomes into numeric
+    output <- as.numeric(output)
+  }
+  
+  n <- ncol(data)
+  classes <- sapply(data, class)
+  yclass <- class(output)
+  
   all_features <- 1:n
-  if(yclass=='factor'){
+  if(yclass=='factor'){ #conduct tests where the outcome is is 2+-level factor
     numerics <- all_features[classes=='numeric']
     factors <- all_features[classes=='factor']
     anova_dat <- data[,numerics]
@@ -20,13 +46,11 @@ uafs.pick <- function(data, output, alpha=0.025){
     picks_1 <- anovapick.1(anova_dat, output, alpha)
     picks_2 <- chi.pick(chi_dat, output, alpha)
   }
-  if(yclass=='numeric'|yclass=='integer'){
+  if(yclass=='numeric'|yclass=='integer'){ #conduct tests between fully continuous data
     numerics <- all_features[classes=='numeric']
-    factors <- all_features[classes=='factor']
     fish_dat <- data[,numerics]
-    anova_dat <- data[,factors]
-    picks_1 <- anovapick.2(anova_dat, output, alpha)
-    picks_2 <- fish.pick(fish_dat, output, alpha)
+    picks_1 <- fish.pick(fish_dat, output, alpha)
+    picks_2 <- numeric(0)
   }
   picks <- c(picks_1, picks_2)
   final_dat <- cbind(output, data[,picks])
